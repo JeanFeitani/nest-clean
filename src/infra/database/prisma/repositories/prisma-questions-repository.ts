54579f4ebda3ad2f980b2554a6c456b969e3345b
jean-feitani <jeanls.folks@gmail.com>
2024-05-11 09:05:10 -0300

    feat: add repositories, question mapper and update prisma schema

---
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -0,0 +1,45 @@
+import { PaginationParams } from '@/core/repositories/paginations-params'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper'
+
+@Injectable()
+export class PrismaQuestionsRepository implements QuestionsRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findById(id: string) {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!question) {
+      return null
+    }
+
+    return PrismaQuestionMapper.toDomain(question)
+  }
+
+  async findBySlug(slug: string): Promise<Question | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  async findManyRecent(params: PaginationParams): Promise<Question[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async create(question: Question): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  async delete(question: Question): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  async save(question: Question): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+}
